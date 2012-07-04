174dae5ca5e75d405255dd23e9486fa228811bf0
Javacraft <frelling@java-craft.com>
2012-07-04 05:13:05 -0400

    Parent POM integration. Added missing constants interface.

---
+++ src/main/java/com/java_craft/mc/JCDiver/IDiverConstants.java
@@ -0,0 +1,9 @@
+package com.java_craft.mc.JCDiver;
+
+public interface IDiverConstants {
+
+	String	P_ENABLED	= "jcdiver.enabled";
+	String	P_UNLIMITED	= "jcdiver.unlimited";
+	short	C_DAMAGE	= 1;
+
+}
